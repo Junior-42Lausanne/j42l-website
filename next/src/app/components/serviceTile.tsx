96a1fe5422d9bfedc4c920c748313c11a730a337
Yokio9 <dmatayi@gmail.com>
2025-12-17 07:09:46 +0100

    made the text on the card start lower

--- next/src/app/components/serviceTile.tsx
+++ next/src/app/components/serviceTile.tsx
@@ -11,16 +11,16 @@ export default function ServiceTile({service}: {service: Service}) {
 		<div
 			className="flex flex-col h-[650px] font-poppins justify-between
 						text-white bg-cover bg-center pt-[10%] pb-[30px] px-[30px] gap-[20px] w-full max-w-[420px]
-						[text-shadow:0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black,0_0_0.5px_black]"
+						[text-shadow:0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black,0_0_1px_black]"
 			style={{backgroundImage:`url("${image}")`}}>
-				<div className="flex flex-col gap-[30px]">
-			<h3 className="text-h3 font-[1000]">{service.heading}</h3>
-			<ul className="list-disc pl-[20px] space-y-2 mb-[20px]">
-			{service.text[0].children.map((item, i) => (
-				<li key={i}>{item.children[0].text}</li>
-			))}
-		</ul>
-		</div>
+			<div className="flex flex-col gap-[30px] mt-[55%]">
+				<h3 className="text-h3 font-[1000]">{service.heading}</h3>
+				<ul className="list-disc pl-[20px] space-y-2 mb-[20px]">
+				{service.text[0].children.map((item, i) => (
+					<li key={i}>{item.children[0].text}</li>
+				))}
+				</ul>
+			</div>
 			<Button text="Découvrir" path={target} color='white' fullWidth={true} />
 		</div>
 	);
