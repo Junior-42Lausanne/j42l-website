1a43519cbd0f435d14224ad1b5df99395fb60f4b
NguyenNguyen <nguyennguyen.dev@protonmail.com>
2026-02-11 17:59:51 +0100

    feature: add sitemap.ts and robots.ts feature: add simple login to staging

---
+++ next/src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+    if (process.env.STAGING === 'true') {
+        return [];
+    }
+    const url : string = "https://j42l.ch";
+    // const url : string = "localhost:3001/";
+    return [
+        {
+            url: `${url}`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: `${url}/about`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
+        {
+            url: `${url}/student`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.5,
+        },{
+            url: `${url}/contact`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.5,
+        },{
+            url: `${url}/web`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },{
+            url: `${url}/prototype`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },{
+            url: `${url}/automation`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+    ]
+}
